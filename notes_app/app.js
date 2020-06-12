bfc417600ee02fa874bc88be7c81cf07d24ae125
Jithin <jithinyohannan@gmail.com>
2020-06-12 03:46:44 -0500

    refined

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,15 +1,3 @@
-/*
-const fs = require('fs')
-
-fs.writeFileSync('notes.txt','The input is by node js!!')
-fs.appendFileSync('notes.txt',', Checking the append sync working');
-*/
-/*
-const hMsg = require('./uitls')
-const welcomeNote= 'Welcome to NodeJS'
-console.log(welcomeNote)
-console.log(hMsg) 
-*/
 
 const getNotes = require('./notes')
 const result = getNotes()
