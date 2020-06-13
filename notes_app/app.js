8a8368bdbec197f71190e2bafeb56d1f24908a7a
Jithin <jithinyohannan@gmail.com>
2020-06-13 03:13:34 -0500

    node modules, nodemon, validator, chalk

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,5 +1,9 @@
 
-const getNotes = require('./notes')
+//const validator = require('validator')
+const chalk = require('chalk')
+
+const getNotes = require('./notes.js')
 const result = getNotes()
 console.log(result)
-
+console.log(chalk.bold.red.inverse('Error!'))
+//console.log(validator.isEmaidirl('checking@gmail.com'))
