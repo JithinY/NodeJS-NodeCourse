747063d74e632cbdf7654ea9604fd4cfe2cb8f06
Jithin <jithinyohannan@gmail.com>
2020-06-15 19:20:22 -0500

    15Jun

--- notes_app/app.js
+++ notes_app/app.js
@@ -5,5 +5,7 @@ const chalk = require('chalk')
 const getNotes = require('./notes.js')
 const result = getNotes()
 console.log(result)
-console.log(chalk.bold.red.inverse('Error!'))
+console.log(chalk.blue.inverse.bold('Success!'))
 //console.log(validator.isEmaidirl('checking@gmail.com'))
+
+console.log(process.argv[2])
