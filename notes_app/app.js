187597def03e52ac5efc3d1eebb6430aa8e71fcb
Jithin <jithinyohannan@gmail.com>
2020-06-16 23:18:01 -0500

    Functionality for adding and list notes created

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,11 +1,51 @@
 
-//const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const notesUtil = require('./notes.js')
 
-const getNotes = require('./notes.js')
-const result = getNotes()
-console.log(result)
-console.log(chalk.blue.inverse.bold('Success!'))
-//console.log(validator.isEmaidirl('checking@gmail.com'))
+yargs.command({
+        command:'add',
+        describe:'To add new note',
+        builder:{
+            title:{
+                describe:'Title for Note',
+                demandOption:true,
+                type: 'string'
+            },
+            body:{
+                describe:'Body for Note',
+                demandOption:true,
+                type: 'string'
+            }
+        },
+        handler: function(argv){
+            notesUtil.addNote(argv.title,argv.body)
+        }
+})
+yargs.command({
+    command:'remove',
+    describe:'To remove new note',
+    handler: function(){
+        console.log('Removing a note!')
+    }
+})
+yargs.command({
+    command:'list',
+    describe:'To list notes',
+    handler: function(){
+        console.log('Listing the notes here..!')
+        const stringJsonData = notesUtil.getNotes()
+        console.log(stringJsonData)
+    }
+})
+yargs.command({
+    command:'read',
+    describe:'To read a note',
+    handler: function(){
+        console.log('Reading a note!')
+    }
+})
 
-console.log(process.argv[2])
+yargs.parse()
+
+
--- notes_app/notes.js
+++ notes_app/notes.js
@@ -1,6 +1,50 @@
 
-const getNoteTxt = function(){
-    return 'Your notes...'
+const fs = require('fs')
+const { Console } = require('console')
+
+const getNotes = function(){
+    const bufferData = fs.readFileSync('notes.json')
+    const stringJsonData = bufferData.toString()
+    return stringJsonData
 }
 
-module.exports = getNoteTxt
+const addNote = function(title, body){
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(
+        function(note){
+            return note.Title === title
+        }
+    )
+
+    if(duplicateNotes.length === 0){
+        console.log('Addding a note!')
+        notes.push(
+            {Title:title,Body:body}
+        )
+        const stringJsonData = JSON.stringify(notes);
+        fs.writeFileSync('notes.json',stringJsonData)
+    }    
+    else{
+        console.log('Duplicate note found with same title!!')
+    }
+    
+}
+
+const loadNotes = function(){
+    try{
+    const bufferData = fs.readFileSync('notes.json')
+    const stringJsonData = bufferData.toString()
+    return JSON.parse(stringJsonData)
+    }
+    catch(e){ 
+        console.log(e.toString())
+        return[]
+    }
+}
+
+const notesUtil = {
+    getNotes:getNotes,
+    addNote:addNote
+}
+
+module.exports = notesUtil
